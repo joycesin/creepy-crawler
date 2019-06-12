c219752b4bdbfbabce13e22c4903b18f1b8774b2
Joyce Sin <joycesin@Joyces-MacBook-Pro.local>
2019-06-12 10:39:29 +0800

    Scraper producing decent results now Reduced seed URLs to 2 Google search results Replaced urllib library with requests library for formatting Google search query Parsed Google search results HTML properly to get only results links hrefs Only populated new links from URLs above a score of 3 Added extra meta keywords/specialty etc to catch more relevant results Added more keywords and high keywords

--- scraper.py
+++ scraper.py
@@ -4,12 +4,19 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import re
+import requests
 
-keywords = ["telecom", "telecoms", "telecommunications", "telco", "email", "AT&T", "Verizon", "Sprint", "T-Mobile", "Huawei", "Vodafone", "Telefonica", "Deutsche", "Telekom", "NTT", "Softbank", "Orange", "BT", "CenturyLink", "Telstra", "KT", "LG", "Ericsson", "Nokia", "acquisition", "merger", "partnership", "operator", "operators", "strategic", "digital", "measures", "measure", "Russia", "China", "US", "UK", "Japan", "Korea", "response", "responses", "provider", "IXP", "ISP", "submarine", "cable", "latency", "latencies", "sensitive", "patent"]
+keywords = ["telecom", "telecoms", "telecommunications", "telco", "email", "AT&T", "Verizon", "Sprint", "T-Mobile", "Huawei", "Vodafone", "Telefonica", "Deutsche", "Telekom", "NTT", "Softbank", "Orange", "BT", "CenturyLink", "Telstra", "KT", "LG", "Ericsson", "Nokia", "acquisition", "merger", "partnership", "operator", "operators", "strategic", "digital", "measures", "measure", "Russia", "China", "US", "UK", "Japan", "Korea", "response", "responses", "provider", "IXP", "ISP", "submarine", "cable", "latency", "latencies", "sensitive", "patent", "broadband", "attack", "attacks"]
 
-high_keywords = ["security", "cybersecurity", "cyber-security", "cyber", "secure", "resiliency", "resilient", "vulnerable", "vulnerabilities", "vulnerability", "threat", "threats", "defend", "phishing", "spoofing", "spoofed", "outage", "cryptography", "hacking", "hacked", "hackers", "malicious", "cyberdefence", "attack", "attacks", "attacked", "attacker", "attackers", "cyber-attack", "cyber-defence", "defence", "defense", "encrypted", "encryption", "encrypted", "SSL", "DNS", "protect", "privacy", "law", "regulation", "regulator", "critical", "failure", "breach", "intelligence", "legislation", "leak", "leaked", "risk", "DDoS", "malware", "backdoor"]
+high_keywords = ["security", "cybersecurity", "cyber-security", "cyber", "secure", "resiliency", "resilient", "vulnerable", "vulnerabilities", "vulnerability", "threat", "threats", "defend", "phishing", "spoofing", "spoofed", "outage", "cryptography", "hacking", "hacked", "hackers", "malicious", "cyberdefence", "attack", "attacks", "attacked", "attacker", "attackers", "cyber-attack", "cyberattack", "cyberattacks", "cyber-attacks", "cyberdefence", "cyber-defence", "defence", "defense", "encrypted", "encryption", "encrypted", "SSL", "DNS", "protect", "privacy", "law", "regulation", "regulator", "critical", "failure", "breach", "intelligence", "legislation", "leak", "leaked", "risk", "DDoS", "malware", "backdoor"]
 
-urls = ["http://www.google.com/search?q=telecom+services+brought+down+cyberattack", "http://www.google.com/search?q=telecom+cyber+security+mitigate+risk", "http://telecoms.com/497227/orange-drops-e515-million-on-yet-another-cybersecurity-acquisition/", "https://www.lightreading.com/carrier-security.asp", "http://www.computerweekly.com/news/252459155/Malaysia-takes-broad-base-approach-to-5G-security", "http://www.computerweekly.com/news/252450417/Optus-to-acquire-Hivint-in-cyber-security-deal", "http://www.upguard.com/blog/how-secure-is-your-cell-phone-provider"]
+urls = ["http://www.google.com/search?q=telecom+services+brought+down+cyberattack", "http://www.google.com/search?q=telecom+cyber+security+mitigate+risk"]
+
+"""
+"https://blog.radware.com/security/2016/03/cyber-security-threats-in-telecom/", 
+
+"http://telecoms.com/497227/orange-drops-e515-million-on-yet-another-cybersecurity-acquisition/", "https://www.lightreading.com/carrier-security.asp", "http://www.computerweekly.com/news/252459155/Malaysia-takes-broad-base-approach-to-5G-security", "http://www.computerweekly.com/news/252450417/Optus-to-acquire-Hivint-in-cyber-security-deal", "http://www.upguard.com/blog/how-secure-is-your-cell-phone-provider"]
+"""
 
 blacklist_strings = ["http://www.allaboutcookies", "http://twitter", "http://jobs", "http://servedby", "http://shop.vodafone", "http://www.vodafone", "http://translate.google.com"]
 
@@ -27,7 +34,7 @@ def start():
     for url in urls:
         links[url] = 0
 
-    while df_count < 30:
+    while df_count < 100:
         # get the top page and pop it off list of links
         if not links:
             print("Links dict is empty")
@@ -43,20 +50,33 @@ def start():
         if not start_page: 
             print("Links dict is all checked")
             break
-        print("trying to open " + start_page)
+
         try: 
-            page = urllib2.urlopen(formatUrl(start_page))
+            #page = urllib2.urlopen(formatUrl(start_page))
+            page = requests.get(start_page, headers={'User-Agent':'Mozilla 5.10'})
+            print("opened " + start_page)
         except:
             print("Could not open start_page")
             continue
 
         # parse html
-        soup = BeautifulSoup(page, 'html.parser')
+        try: 
+            soup = BeautifulSoup(page.text, "html.parser")
+        except:
+            print("Could not make soup")
+            continue
 
         # handle google search cases
         if start_page.startswith("http://www.google"):
-            for link in soup.select('.r a'):
-                new_links[link.get('href')] = 0
+            #print (soup.prettify())
+            for link in soup.find_all('a'):
+                try:
+                    new_link = link.get('href').split("/url?q=")[1].split("&sa=")[0]
+                    print("FOUND LINK!!")
+                    new_links[new_link] = 0
+                except:
+                    continue
+ 
         else:
 
             # get words in title
@@ -74,7 +94,7 @@ def start():
             score += checkRelevance(tag_list) 
             score += checkRelevance(title_list)
 
-
+            # only non-google search results will affect score, google results are just to populate new_links
             # appends url to df if score > 0, can adjust criteria later
             if score >= 3:
                 df.loc[df_count] = [start_page, score]
@@ -82,7 +102,8 @@ def start():
                 df.sort_values("score")
                 print df
                 export_csv = df.to_csv(r'/Users/joycesin/Documents/crawler/urls.csv', index = None, header=True)
-            new_links = getLinks(soup)
+                # only populate new links from seed urls above certain score
+                new_links = getLinks(soup)
 
         # end if-else, update overall links dict with new_links
         # update list of links without duplicates, even for urls that weren't added to df cause they might be search result pages etc
@@ -96,7 +117,13 @@ def start():
 # get a list of meta tags
 def getMeta(soup):
     tag_list = set()
+
+    print(soup.find_all("script", src=re.compile("cyberattack")))
+
     tags = soup.find_all("meta", property="article:tag")
+    tags.extend(soup.find_all("meta", itemprop="keywords"))
+    tags.extend(soup.find_all("meta", itemprop="specialty"))
+    tags.extend(soup.select("meta", name="keywords"))
     for tag in tags:
         tag_list.add(tag.get("content", None))
     return tag_list
@@ -123,7 +150,7 @@ def check_blacklist(new_link):
 def getLinks(soup):
     new_links = {}
 
-    for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+    for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
         # filter out twitter, other junk pages
         new_link = link.get('href')
         if check_blacklist(new_link):

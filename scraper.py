74aab27223c272a6e31883a514ae513787eaae8b
Joyce Sin <joycesin@Joyces-MacBook-Pro.local>
2019-06-13 15:57:56 +0800

    clean up print statements

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import urllib2
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
@@ -10,19 +9,8 @@ keywords = ["telecom", "telecoms", "telecommunications", "telco", "AT&T", "Veriz
 
 high_keywords = ["security", "cybersecurity", "cyber-security", "cyber", "secure", "resiliency", "resilient", "vulnerable", "vulnerabilities", "vulnerability", "threat", "threats", "defend", "phishing", "spoofing", "spoofed", "outage", "cryptography", "hacking", "hacked", "hackers", "malicious", "cyberdefence", "attack", "attacks", "attacked", "attacker", "attackers", "cyber-attack", "cyberattack", "cyberattacks", "cyber-attacks", "cyberdefence", "cyber-defence", "defence", "defense", "encrypted", "encryption", "encrypted", "SSL", "DNS", "protect", "privacy", "law", "regulation", "regulator", "critical", "failure", "breach", "intelligence", "leak", "leaked", "risk", "DDoS", "malware", "backdoor"]
 
+# SEED URLS
 urls = ["http://www.google.com/search?q=telecom+services+brought+down+cyberattack", "http://www.google.com/search?q=telecom+cyber+security+mitigate+risk"]
-
-"""
-"https://blog.radware.com/security/2016/03/cyber-security-threats-in-telecom/", 
-"http://telecoms.com/497227/orange-drops-e515-million-on-yet-another-cybersecurity-acquisition/", "https://www.lightreading.com/carrier-security.asp", "http://www.computerweekly.com/news/252459155/Malaysia-takes-broad-base-approach-to-5G-security", "http://www.computerweekly.com/news/252450417/Optus-to-acquire-Hivint-in-cyber-security-deal", "http://www.upguard.com/blog/how-secure-is-your-cell-phone-provider"]
-"""
-
-blacklist_strings = ["http://www.allaboutcookies", "http://twitter", "http://jobs", "http://servedby", "http://shop.vodafone", "http://www.vodafone", "http://translate.google.com"]
-
-def formatUrl(new_url):
-    headers = {'User-Agent':'Mozilla 5.10'}
-    request = urllib2.Request(new_url, None, headers)
-    return request
 
 def start():
     df_count = 0
@@ -36,7 +24,7 @@ def start():
     while df_count < 100:
         # get the top page and pop it off list of links
         if not links:
-            print("Links dict is empty")
+            print("Links dictionary is empty")
             break
         start_page = ""
         new_links = {}
@@ -47,11 +35,10 @@ def start():
                 links[x] = 1
                 break
         if not start_page: 
-            print("Links dict is all checked")
+            print("Links dictionary is all checked")
             break
 
         try: 
-            #page = urllib2.urlopen(formatUrl(start_page))
             page = requests.get(start_page, headers={'User-Agent':'Mozilla 5.10'})
             print("opened " + start_page)
         except:
@@ -67,17 +54,14 @@ def start():
 
         # handle google search cases
         if start_page.startswith("http://www.google"):
-            #print (soup.prettify())
             for link in soup.find_all('a'):
                 try:
                     new_link = link.get('href').split("/url?q=")[1].split("&sa=")[0]
-                    print("FOUND LINK!!")
                     new_links[new_link] = 0
                 except:
                     continue
  
         else:
-
             # get words in title
             title_list = []
             try:
@@ -95,25 +79,21 @@ def start():
             score = 0
             score += checkRelevance(tag_list) 
 
-            # only non-google search results will affect score, google results are just to populate new_links
+            # only non-google search results will affect score
             # appends url to df if score > 0, can adjust criteria later
             if score >= 3:
                 df.loc[df_count] = [start_page, score]
                 df_count += 1
                 df.sort_values("score")
                 print df
+                # TARGET CSV LOCATION on local machine
                 export_csv = df.to_csv(r'/Users/joycesin/Documents/crawler/urls.csv', index = None, header=True)
-                # only populate new links from seed urls above certain score
+                # only populates new links from seed urls above certain score
                 new_links = getLinks(soup)
 
-        # end if-else, update overall links dict with new_links
-        # update list of links without duplicates, even for urls that weren't added to df cause they might be search result pages etc
+        # update overall links dict with new_links, avoiding duplicates
         new_links.update(links)
         links = new_links
-    
-
-
-
 
 # get a list of meta tags
 def getMeta(soup):
@@ -127,8 +107,7 @@ def getMeta(soup):
         tag_list.add(tag.get("content", None))
     return tag_list
 
-
-# expects a list of words to check against list of keywords, scores out of 10 matches
+# expects a list of words to check against list of keywords, gives a score
 def checkRelevance(tags):
     score = 0
     for tag in tags:
@@ -140,11 +119,6 @@ def checkRelevance(tags):
             score += 2            
     return score
 
-def check_blacklist(new_link):
-    for blacklist in blacklist_strings:
-        if new_link.startswith(blacklist):
-            return True
-    return False
 # returns dict with keys as urls and values as 0 -- unchecked
 def getLinks(soup):
     new_links = {}
@@ -152,10 +126,7 @@ def getLinks(soup):
     for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
         # filter out twitter, other junk pages
         new_link = link.get('href')
-        if check_blacklist(new_link):
-            continue
         new_links[new_link] = 0
     return new_links
-
 
 start()
